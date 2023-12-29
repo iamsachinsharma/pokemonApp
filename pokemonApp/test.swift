417ce940b0cb263a7b6154f2da2d282447ed9644
Sachin <sachinsharma2168@gmail.com>
2023-12-29 22:39:47 +0530

    splash screen

---
+++ pokemonApp/test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  pokemonApp
+//
+//  Created by Sachin Sharma on 29/12/23.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    test()
+}
